b7790a1345a688eeb506b32ad5a176fe6d7e48b4
David Albert <davidbalbert@gmail.com>
2014-12-28 13:19:54 -0500

    Save runtime.rs for later

---
+++ kernel/runtime.rs
@@ -0,0 +1,136 @@
+use util;
+
+#[no_mangle]
+pub fn floorf() {
+    util::die("No `floorf'");
+}
+
+#[no_mangle]
+pub fn ceilf() {
+    util::die("No `ceilf'");
+}
+
+#[no_mangle]
+pub fn roundf() {
+    util::die("No `roundf'");
+}
+
+#[no_mangle]
+pub fn truncf() {
+    util::die("No `truncf'");
+}
+
+#[no_mangle]
+pub fn fmaf() {
+    util::die("No `fmaf'");
+}
+
+#[no_mangle]
+pub fn __powisf2() {
+    util::die("No `__powisf2'");
+}
+
+#[no_mangle]
+pub fn powf() {
+    util::die("No `powf'");
+}
+
+#[no_mangle]
+pub fn expf() {
+    util::die("No `expf'");
+}
+
+#[no_mangle]
+pub fn exp2f() {
+    util::die("No `exp2f'");
+}
+
+#[no_mangle]
+pub fn logf() {
+    util::die("No `logf'");
+}
+
+#[no_mangle]
+pub fn log2f() {
+    util::die("No `log2f'");
+}
+
+#[no_mangle]
+pub fn log10f() {
+    util::die("No `log10f'");
+}
+
+#[no_mangle]
+pub fn floor() {
+    util::die("No `floor'");
+}
+
+#[no_mangle]
+pub fn ceil() {
+    util::die("No `ceil'");
+}
+
+#[no_mangle]
+pub fn round() {
+    util::die("No `round'");
+}
+
+#[no_mangle]
+pub fn trunc() {
+    util::die("No `trunc'");
+}
+
+#[no_mangle]
+pub fn fma() {
+    util::die("No `fma'");
+}
+
+#[no_mangle]
+pub fn pow() {
+    util::die("No `pow'");
+}
+
+#[no_mangle]
+pub fn __powidf2() {
+    util::die("No `__powidf2'");
+}
+
+#[no_mangle]
+pub fn exp() {
+    util::die("No `exp'");
+}
+
+#[no_mangle]
+pub fn exp2() {
+    util::die("No `exp2'");
+}
+
+#[no_mangle]
+pub fn log() {
+    util::die("No `log'");
+}
+
+#[no_mangle]
+pub fn log2() {
+    util::die("No `log2'");
+}
+
+#[no_mangle]
+pub fn log10() {
+    util::die("No `log10'");
+}
+
+#[no_mangle]
+pub fn fmodf() {
+    util::die("No `fmodf'");
+}
+
+#[no_mangle]
+pub fn fmod() {
+    util::die("No `fmod'");
+}
+
+#[no_mangle]
+pub fn memcmp() {
+    util::die("No `memcmp'");
+}
